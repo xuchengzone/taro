2327486bb4b86cf7cdeab9a1407b7aba02ec53f7
xuchengzone <xuchengzone@gmail.com>
2020-08-28 09:43:38 +0800

    RouterInfo add shareTicket scene from app.tsx
    RouterInfo add shareTicket scene from app.tsx

--- packages/taro/types/taro.component.d.ts
+++ packages/taro/types/taro.component.d.ts
@@ -52,6 +52,10 @@ declare namespace Taro {
     onReady: string
     onHide: string
     onShow: string
+    
+    shareTicket:string|undefined
+    scene:number|undefined
+
   }
 
   interface Component<P = {}, S = {}, SS = any> extends ComponentLifecycle<P, S, SS> {
